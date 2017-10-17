0338a1ae040d7d0968a3d5961f5e06f924510afc
zsx47 <thezisman@gmail.com>
2017-10-17 16:31:52 -0400

    Removed some unneeded stuff.

--- src/main/java/net/thisisz/hermes/bungee/HermesChatListener.java
+++ src/main/java/net/thisisz/hermes/bungee/HermesChatListener.java
@@ -3,12 +3,7 @@ package net.thisisz.hermes.bungee;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.connection.Server;
 import net.md_5.bungee.api.event.ChatEvent;
-import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.event.EventHandler;
-import net.thisisz.hermes.bungee.asynctask.LoadPlayerThenCallback;
-import net.thisisz.hermes.bungee.callback.PlayerLoginNotification;
-import net.thisisz.hermes.bungee.asynctask.SendLogoutNotification;
 import net.thisisz.hermes.bungee.messaging.local.provider.LocalProvider;
 import net.thisisz.hermes.bungee.messaging.tasks.SendChatMessage;
 
